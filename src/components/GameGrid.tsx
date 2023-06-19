58adf4007f6fa48da0feee85e539bc3ab9f1a85e
Aki Sirkiä <74837175+lekatehdas@users.noreply.github.com>
2023-06-19 08:47:27 +0200

    Refactor: remove duplicated styles

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,6 +2,7 @@ import {SimpleGrid, Text} from "@chakra-ui/react";
 import useGames from "../hooks/useGames.ts";
 import GameCard from "./GameCard.tsx";
 import GameCardSkeleton from "./GameCardSkeleton.tsx";
+import GameCardContainer from "./GameCardContainer.tsx";
 
 const GameGrid = () => {
     const {games, error, isLoading} = useGames()
@@ -11,12 +12,15 @@ const GameGrid = () => {
         <>
             {error && <Text>{error}</Text>}
             <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} spacing={10} padding={"10px"}>
-                {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton}/>)}
+                {isLoading && skeletons.map(skeleton => (
+                    <GameCardContainer>
+                        <GameCardSkeleton key={skeleton}/>
+                    </GameCardContainer>))}
                 {games.map(game =>
-                    <GameCard
+                    <GameCardContainer><GameCard
                         key={game.id}
                         game={game}
-                    />
+                    /></GameCardContainer>
                 )}
             </SimpleGrid>
         </>
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '2f35c75a180e488ab7c52a24413dad40'
+        key: '78825f15cd974fa2a6c80f144849e4bf'
     }
 })
