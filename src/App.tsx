959b9d481269d66460240b00caba21546b5bb5c0
Aki Sirkiä <74837175+lekatehdas@users.noreply.github.com>
2023-07-02 21:51:25 +0200

    Fixed issue with chakra menus

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {Grid, GridItem, HStack, Show} from '@chakra-ui/react'
+import {Box, Flex, Grid, GridItem, Show} from '@chakra-ui/react'
 import NavBar from "./components/NavBar.tsx";
 import GameGrid from "./components/GameGrid.tsx";
 import GenreList from "./components/GenreList.tsx";
@@ -42,10 +42,14 @@ function App() {
         </Show>
 
         <GridItem area='main'>
-            <HStack spacing={5} paddingLeft={2} marginBottom={5}>
-                <PlatFormSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})}/>
-                <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery, sortOrder})}/>
-            </HStack>
+            <Flex paddingLeft={2} marginBottom={5}>
+                <Box marginRight={5}>
+                    <PlatFormSelector selectedPlatform={gameQuery.platform}
+                                      onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})}/>
+                </Box>
+                <SortSelector sortOrder={gameQuery.sortOrder}
+                              onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery, sortOrder})}/>
+            </Flex>
 
             <GameGrid gameQuery={gameQuery}/>
         </GridItem>
