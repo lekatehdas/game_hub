e8099a6f63f323e75dc9af0825e85332089025ca
Aki Sirkiä <74837175+lekatehdas@users.noreply.github.com>
2023-12-26 23:12:42 +0100

    Implemented infinite queries

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,6 @@
 import { Genre } from "./useGenres.ts";
 import { GameQuery } from "../App.tsx";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import ApiClient, { FetchResponse } from "../services/api-client.ts";
 import { Platform } from "./usePlatforms.ts";
 
@@ -17,17 +17,21 @@ export interface Game {
 }
 
 const useGames = (gameQuery: GameQuery) =>
-	useQuery<FetchResponse<Game>, Error>({
+	useInfiniteQuery<FetchResponse<Game>, Error>({
 		queryKey: ["games", gameQuery],
-		queryFn: () =>
+		queryFn: ({ pageParam = 1 }) =>
 			apiClient.getAll({
 				params: {
 					genres: gameQuery.genre?.id,
 					parent_platforms: gameQuery.platform?.id,
 					ordering: gameQuery.sortOrder,
 					search: gameQuery.searchText,
+					page: pageParam,
 				},
 			}),
+		getNextPageParam: (lastPage, allPages) => {
+			return lastPage.next ? allPages.length + 1 : undefined;
+		},
 	});
 
 export default useGames;
