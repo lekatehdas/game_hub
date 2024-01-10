cf1b8b51cd354997a28caf2d8f346404a4fa6a31
Aki Sirkiä <74837175+lekatehdas@users.noreply.github.com>
2024-01-10 23:15:45 +0100

    Build - game trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { Heading, Spinner } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText.tsx";
 import GameAttributes from "../components/GameAttributes.tsx";
+import GameTrailer from "../components/GameTrailer.tsx";
 
 const GameDetailPage = () => {
 	const { slug } = useParams();
@@ -21,6 +22,7 @@ const GameDetailPage = () => {
 			<Heading>{game.name}</Heading>
 			<ExpandableText>{game.description_raw}</ExpandableText>
 			<GameAttributes game={game} />
+			<GameTrailer gameId={game.id} />
 		</>
 	);
 };
